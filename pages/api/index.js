dc60ea4dc1ec74a41fbd0d9b0badcb0774160f48
Evan Silverman <es4753@nyu.edu>
2020-09-18 15:57:45 -0400

    add schedule endpoints

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 export default (req, res) => {
   const filePath = path.resolve('./public/docs.html');
   const stat = fs.statSync(filePath);
+
   res.statusCode = 200;
   res.writeHead(200, {
     'Content-Type': 'text/html',
--- pages/api/v1/teams/[id]/schedule.js
+++ pages/api/v1/teams/[id]/schedule.js
@@ -1,7 +1,7 @@
 import SQL from 'sql-template-strings';
 import Cors from 'cors';
-import { query } from '../../../../lib/db';
-import use from '../../../../lib/middleware';
+import { query } from '../../../../../lib/db';
+import use from '../../../../../lib/middleware';
 
 const cors = Cors({
   methods: ['GET', 'HEAD'],
@@ -9,6 +9,14 @@ const cors = Cors({
 
 export default async (req, res) => {
   await use(req, res, cors);
+
+  const id = parseInt(req.query.id, 10);
+
+  if (Number.isNaN(id)) {
+    res.status(400).send('Error: id must be a number');
+    return;
+  }
+
   const league = parseInt(req.query.league, 10) || 0;
   const [season] =
     (!Number.isNaN(parseInt(req.query.season, 10)) && [
@@ -27,6 +35,7 @@ export default async (req, res) => {
     FROM schedules
     WHERE LeagueID=${league}
       AND SeasonID=${season.SeasonID}
+      AND (Home=${id} OR Away=${id})
   `);
 
   const parsed = schedule.map((game) => ({
