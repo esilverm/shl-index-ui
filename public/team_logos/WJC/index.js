87cbf9873e86c3381c7a287f7595203d4217143c
Evan Silverman <es4753@nyu.edu>
2020-09-23 12:23:35 -0400

    add WJC logos

---
+++ public/team_logos/WJC/index.js
@@ -0,0 +1,19 @@
+import BRI from './BritishIsles.svg?sprite';
+import CAN from '../IIHF/Canada.svg?sprite';
+import DCH from './Dach.svg?sprite';
+import FIN from '../IIHF/Finland.svg?sprite';
+import RCL from './UCORCAL.svg?sprite';
+import SWE from '../IIHF/Sweden.svg?sprite';
+import USA from '../IIHF/USA.svg?sprite';
+import WOR from './World.svg?sprite';
+
+export default {
+  BRI,
+  CAN,
+  DCH,
+  FIN,
+  RCL,
+  SWE,
+  USA,
+  WOR,
+};
