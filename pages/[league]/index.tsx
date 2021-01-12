40d3ef6d9e7a6a328b1dfbf0d6fe5c07a38c3a92
Evan Silverman <es4753@nyu.edu>
2021-01-12 14:25:48 -0500

    make league index the standings

--- pages/[league]/index.tsx
+++ pages/[league]/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { NextSeo } from 'next-seo';
 import { GetStaticProps, GetStaticPaths } from 'next';
+import { NextSeo } from 'next-seo';
 
 import Header from '../../components/Header';
+import StandingsTable from '../../components/StandingsTable';
+import useStandings from '../../hooks/useStandings';
 
 interface Props {
   league: string;
 }
 
 function LeagueHome({ league }: Props): JSX.Element {
+  const [display, setDisplay] = useState('league');
+  const { standings, isLoading } = useStandings(league, display);
+
   return (
     <React.Fragment>
       <NextSeo
@@ -20,20 +25,104 @@ function LeagueHome({ league }: Props): JSX.Element {
       />
       <Header league={league} />
       <Container>
-        <h1>Text about Hockey</h1>
+        <DisplaySelectContainer role="tablist">
+          <DisplaySelectItem
+            onClick={() => setDisplay(() => 'league')}
+            active={display === 'league'}
+            tabIndex={0}
+            role="tab"
+            aria-selected={display === 'league'}
+          >
+            League
+          </DisplaySelectItem>
+          <DisplaySelectItem
+            onClick={() => setDisplay(() => 'conference')}
+            active={display === 'conference'}
+            tabIndex={0}
+            role="tab"
+            aria-selected={display === 'conference'}
+          >
+            Conference
+          </DisplaySelectItem>
+          {league !== 'iihf' && league !== 'wjc' && (
+            <DisplaySelectItem
+              onClick={() => setDisplay(() => 'division')}
+              active={display === 'division'}
+              tabIndex={0}
+              role="tab"
+              aria-selected={display === 'division'}
+            >
+              Division
+            </DisplaySelectItem>
+          )}
+        </DisplaySelectContainer>
+        <StandingsTableWrapper>
+          {Array.isArray(standings) && 'teams' in standings[0] && !isLoading ? (
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            standings.map((group, i) => (
+              <StandingsTableContainer key={i}>
+                <StandingsTable
+                  data={group.teams}
+                  league={league}
+                  title={group.name}
+                  isLoading={isLoading}
+                />
+              </StandingsTableContainer>
+            ))
+          ) : (
+            <StandingsTable
+              data={standings}
+              league={league}
+              isLoading={isLoading}
+            />
+          )}
+        </StandingsTableWrapper>
       </Container>
     </React.Fragment>
   );
 }
 
 const Container = styled.div`
-  height: 150vh;
   width: 75%;
-  margin: 60px auto;
-  @media screen and (max-width: 768px) {
+  padding: 1px 0 40px 0;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.colors.grey100};
+
+  @media screen and (max-width: 1024px) {
     width: 95%;
-    padding: 0 2.5%;
+    padding: 2.5%;
   }
+`;
+
+const DisplaySelectContainer = styled.div`
+  margin: 40px auto;
+  width: 95%;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey500};
+`;
+
+const DisplaySelectItem = styled.div<{ active: boolean }>`
+  display: inline-block;
+  padding: 8px 32px;
+  border: 1px solid
+    ${({ theme, active }) => (active ? theme.colors.grey500 : 'transparent')};
+  background-color: ${({ theme, active }) =>
+    active ? theme.colors.grey100 : 'transparent'};
+  border-radius: 5px 5px 0 0;
+  cursor: pointer;
+  position: relative;
+  border-bottom: none;
+  bottom: -1px;
+`;
+
+const StandingsTableWrapper = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+
+const StandingsTableContainer = styled.div`
+  width: 100%;
+  margin: 30px 0;
 `;
 
 export const getStaticPaths: GetStaticPaths = async () => {
