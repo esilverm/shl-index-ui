b9ea184aa3955597b9109d23d19d71228fa7e932
Evan Silverman <es4753@nyu.edu>
2021-03-12 18:03:52 -0500

    add leader api routes

---
+++ pages/api/v1/leaders/goalies/gaa.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import SQL from 'sql-template-strings';
+import Cors from 'cors';
+import { query } from '../../../../../lib/db';
+import use from '../../../../../lib/middleware';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  await use(req, res, cors);
+
+  const { league = 0, season: seasonid, type: shorttype = 'rs', limit = 10, desc = false } = req.query;
+
+  let type: string;
+  if (shorttype === 'po' || shorttype === 'ps' || shorttype === 'rs') {
+    type = shorttype;
+  } else {
+    type = 'rs';
+  }
+
+  const [season] =
+    (seasonid !== undefined &&
+      !Number.isNaN(seasonid) && [{ SeasonID: seasonid }]) ||
+    (await query(
+      SQL`
+      SELECT DISTINCT SeasonID
+      FROM `.append(`player_skater_stats_${type}`).append(SQL`
+      WHERE LeagueID=${+league}
+      ORDER BY SeasonID DESC
+      LIMIT 1
+  `)
+    ));
+
+  // ? Using Minutes > 60 to remove goalies that have only played part of a game.
+  const gaaLeaders = await query(SQL`
+    SELECT s.PlayerID, s.LeagueID, s.SeasonID, s.TeamID, p.\`Last Name\` AS Name, s.GAA
+    FROM `.append(`player_goalie_stats_${type} AS s`).append(SQL`
+    INNER JOIN player_master as p
+      ON s.SeasonID = p.SeasonID 
+      AND s.LeagueID = p.LeagueID
+      AND s.PlayerID = p.PlayerID
+    WHERE s.LeagueID=${+league}
+    AND s.SeasonID=${season.SeasonID}
+    AND s.Minutes > 60
+    ORDER BY s.GAA `).append(desc ? `DESC` : `ASC`).append(`
+    LIMIT ${limit}
+    `));
+ 
+  const parsed = [...gaaLeaders].map((player) => ({
+    id: player.PlayerID,
+    name: player.Name,
+    league: player.LeagueID,
+    team: player.TeamID,
+    season: player.SeasonID,
+    gaa: player.GAA,
+  }))
+
+
+  res.status(200).json(parsed);
+};
