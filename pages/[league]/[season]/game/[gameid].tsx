5a55821a5974278ebbd1062b591be52af36e6e31
Kenny Gilles <kenny.gilles@gmail.com>
2021-04-05 11:14:03 -0400

    Create base for game page

--- pages/[league]/[season]/game/[gameid].tsx
+++ pages/[league]/[season]/game/[gameid].tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
+import { NextSeo } from 'next-seo';
 
-function GameResults(): JSX.Element {
-  return <div>This is the placeholder</div>;
+import Header from '../../../../components/Header';
+
+interface Props {
+  league: number;
+  gameId: string;
+}
+
+function GameResults({ league, gameId }: Props): JSX.Element {
+  console.log(league, gameId);
+  return (
+    <React.Fragment>
+      <NextSeo
+        title='Game'
+        openGraph={{
+          title: 'Game',
+        }}
+      />
+      <Header league={league} />
+      <div>This is a placeholder</div>
+    </React.Fragment>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { league, gameId } = params;
+  const leagues = ['shl', 'smjhl', 'iihf', 'wjc'];
+  const leagueId = leagues.indexOf(league as string);
+
+  return { props: { league: leagueId, gameId } };
 }
 
 export default GameResults;
