375f6c9884ce3ed5c64378af6bfcaf80139cd2d3
Kenny Gilles <kenny.gilles@gmail.com>
2021-04-05 12:20:45 -0400

    Update new path

--- pages/[league]/[season]/game/[gameId].tsx
+++ pages/[league]/[season]/game/[gameId].tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { GetServerSideProps } from 'next';
 import { NextSeo } from 'next-seo';
 import useSWR from 'swr';
+import styled from 'styled-components';
 
 import Header from '../../../../components/Header';
-import { Matchup } from '../../../api/v1/schedule/[gameId]';
+import { Matchup as MatchupData } from '../../../api/v1/schedule/[gameId]';
 
 interface Props {
   league: string;
@@ -12,7 +13,7 @@ interface Props {
 }
 
 function GameResults({ league, gameId }: Props): JSX.Element {
-  const { data, error } = useSWR<Matchup>(
+  const { data, error } = useSWR<MatchupData>(
     `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/schedule/${gameId}`
   );
   console.log(data, error);
@@ -25,10 +26,81 @@ function GameResults({ league, gameId }: Props): JSX.Element {
         }}
       />
       <Header league={league} />
-      <div>This is a placeholder</div>
+      <Container>
+        <TeamStats></TeamStats>
+        <Comparison>
+          <Result>
+            <TeamData>
+              <TeamLogo></TeamLogo>
+              <TeamName></TeamName>
+              <TeamRecord></TeamRecord>
+            </TeamData>
+            <TeamData>
+              <TeamLogo></TeamLogo>
+              <TeamName></TeamName>
+              <TeamRecord></TeamRecord>
+            </TeamData>
+          </Result>
+        </Comparison>
+        <PreviousMatchups>
+          <Matchup></Matchup>
+        </PreviousMatchups>
+      </Container>
     </React.Fragment>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// Left
+const TeamStats = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// Middle
+const Comparison = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Result = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TeamData = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TeamLogo = styled.div`
+  width: 50px;
+  height: 50px;
+`;
+
+const TeamName = styled.span`
+
+`;
+
+const TeamRecord = styled.span`
+
+`;
+
+// Right
+const PreviousMatchups = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// Component?
+const Matchup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { league, gameId } = params;
