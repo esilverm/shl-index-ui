428de2561c70795b1017cf4fab55bb3c3c125ef7
Evan Silverman <es4753@nyu.edu>
2021-04-10 18:00:09 -0400

    modify gameid api query so that it takes 1/8th the time to run

--- pages/api/v1/schedule/game/[gameId].ts
+++ pages/api/v1/schedule/game/[gameId].ts
@@ -9,44 +9,12 @@ const cors = Cors({
   methods: ['GET', 'HEAD'],
 });
 
-interface TeamRow {
-  TeamID: number;
-  LeagueID: number;
-  SeasonID: number;
-  Name: string;
-  Nickname: string;
-  Abbr: string;
-  ParentTeam1: number;
-  ParentTeam2: number;
-  ParentTeam3: number;
-  ParentTeam4: number;
-  ParentTeam5: number;
-  ParentTeam6: number;
-  ParentTeam7: number;
-  ParentTeam8: number;
-  PrimaryColor: string;
-  SecondaryColor: string;
-  TextColor: string;
-  ConferenceID: number;
-  DivisionID: number;
-}
 
 interface TeamRecordRow {
-  ConferenceID: number;
-  DivisionID: number;
-  GA: number;
-  GF: number;
-  LeagueID: number;
+  Wins: number
   Losses: number;
   OTL: number;
-  PCT: number;
-  Points: number;
   SOL: number;
-  SOW: number;
-  SeasonID: number;
-  TeamID: number;
-  Ties: number;
-  Wins: number;
 }
 
 interface TeamStats {
@@ -57,8 +25,9 @@ interface TeamStats {
 }
 
 interface TeamIdentity {
+  id: number,
   name: string;
-  nickName: string;
+  nickname: string;
   abbr: string;
 }
 
@@ -77,11 +46,7 @@ export interface Matchup {
 
 const parseGamesPlayed = (record: TeamRecordRow) => record ? record.Wins + record.Losses + record.OTL + record.SOL : 0;
 const parseTeamRecord = (record: TeamRecordRow) => record ? `${record.Wins}-${record.Losses}-${record.OTL + record.SOL}` : '0-0-0';
-const convertTeamRowToTeamIdentity = (team: TeamRow): TeamIdentity => ({
-  name: team.Name,
-  nickName: team.Nickname,
-  abbr: team.Abbr
-});
+
 
 export default async (
   req: NextApiRequest,
@@ -90,50 +55,34 @@ export default async (
   await use(req, res, cors);
 
   const { gameId } = req.query;
+
   const gameSearch = SQL`
-    SELECT *
-    FROM slugviewer
-    WHERE Slug=${gameId}
+  SELECT DISTINCT s.Slug, s.SeasonID, s.LeagueID, s.Type, s.Date, s.Home, t1.Name as 'HomeName', t1.Nickname as 'HomeNickname', t1.abbr AS 'HomeAbbr', tr1.Wins as 'HomeWins', tr1.Losses as 'HomeLosses', tr1.OTL as 'HomeOTL', tr1.SOW as 'HomeSOW', tr1.SOL as 'HomeSOL', tr1.GF as 'HomeGF', tr1.GA as 'HomeGA', s.HomeScore, s.Away, t2.Name as 'AwayName', t2.Nickname as 'AwayNickname', t2.abbr AS 'AwayAbbr',  tr2.Wins as 'AwayWins', tr2.Losses as 'AwayLosses', tr2.OTL as 'AwayOTL', tr2.SOW as 'AwaySOW', tr2.SOL as 'AwaySOL', tr2.GF as 'AwayGF', tr2.GA as 'AwayGA', s.AwayScore, s.Overtime, s.Shootout, s.Played
+    FROM slugviewer AS s
+    INNER JOIN team_data as t1
+      ON t1.TeamID = s.Home 
+      AND t1.LeagueID = s.LeagueID 
+      AND t1.SeasonID = s.SeasonID
+    INNER JOIN team_data AS t2 
+      ON t2.TeamID = s.Away 
+        AND t2.LeagueID = s.LeagueID 
+        AND t2.SeasonID = s.SeasonID
+    INNER JOIN team_records AS tr1
+      ON tr1.TeamID = s.Home
+      AND tr1.LeagueID = s.LeagueID 
+      AND tr1.SeasonID = s.SeasonID
+    INNER JOIN team_records AS tr2
+      ON tr2.TeamID = s.Away 
+        AND tr2.LeagueID = s.LeagueID 
+        AND tr2.SeasonID = s.SeasonID
+    INNER JOIN slugviewer
+    WHERE s.Slug=${gameId}
   `;
 
-  const game: Array<GameRow> = await query(gameSearch);
-  if (game.length < 1) {
-    return res.status(404).json(`No game found with id ${gameId}`);
-  } else if (game.length > 1) {
-    return res.status(500).json(`More than one game matches id ${gameId}`);
-  }
+  const [game] = await query(gameSearch);
 
-  const relevantGame = game[0];
-  const { SeasonID, LeagueID, Away, Home, Date } = relevantGame;
+  const { SeasonID, LeagueID, Away, Home, Date, Type } = game;
 
-  const awayTeamSearch = SQL`
-    SELECT *
-    FROM team_data
-    WHERE SeasonID=${SeasonID}
-      AND LeagueID=${LeagueID}
-      AND TeamId=${Away};
-  `;
-  const homeTeamSearch = SQL`
-    SELECT *
-    FROM team_data
-    WHERE SeasonID=${SeasonID}
-      AND LeagueID=${LeagueID}
-      AND TeamId=${Home};
-  `;
-  const awayRecordSearch = SQL`
-    SELECT *
-    FROM team_records
-    WHERE SeasonID=${SeasonID}
-      AND LeagueID=${LeagueID}
-      AND TeamId=${Away};
-  `;
-  const homeRecordSearch = SQL`
-    SELECT *
-    FROM team_records
-    WHERE SeasonID=${SeasonID}
-      AND LeagueID=${LeagueID}
-      AND TeamId=${Home};
-  `;
   const previousMatchupsSearch = SQL`
     SELECT *
     FROM slugviewer
@@ -142,34 +91,76 @@ export default async (
       AND (Home=${Away} OR Away=${Away})
       AND (Home=${Home} OR Away=${Home})
       AND CAST(Date as DATE) < ${Date}
+      AND Type=${Type}
     ORDER BY CAST(Date as DATE) DESC;
   `;
 
-  const awayTeamData: Array<TeamRow> = await query(awayTeamSearch);
-  const homeTeamData: Array<TeamRow> = await query(homeTeamSearch);
-  const awayRecordData: Array<TeamRecordRow> = await query(awayRecordSearch);
-  const homeRecordData: Array<TeamRecordRow> = await query(homeRecordSearch);
   const previousMatchups: Array<GameRow> = await query(previousMatchupsSearch);
+
   const awayStats: TeamStats = {
-    gamesPlayed: parseGamesPlayed(awayRecordData[0]),
-    goalsAgainst: awayRecordData[0].GA,
-    goalsFor: awayRecordData[0].GF,
-    record: parseTeamRecord(awayRecordData[0])
+    gamesPlayed: parseGamesPlayed({ 
+      Wins: game.AwayWins, 
+      Losses: game.AwayLosses,
+      OTL: game.AwayOTL,
+      SOL: game.AwaySOL,
+    }),
+    goalsFor: game.AwayGF,
+    goalsAgainst: game.AwayGA,
+    record: parseTeamRecord({ 
+      Wins: game.AwayWins, 
+      Losses: game.AwayLosses,
+      OTL: game.AwayOTL,
+      SOL: game.AwaySOL,
+    }),
   };
+
   const homeStats: TeamStats = {
-    gamesPlayed: parseGamesPlayed(homeRecordData[0]),
-    goalsAgainst: homeRecordData[0].GA,
-    goalsFor: homeRecordData[0].GF,
-    record: parseTeamRecord(homeRecordData[0])
-  };
-  const parsedGame = convertGameRowToGame(relevantGame);
+    gamesPlayed: parseGamesPlayed({ 
+      Wins: game.HomeWins, 
+      Losses: game.HomeLosses,
+      OTL: game.HomeOTL,
+      SOL: game.HomeSOL,
+    }),
+    goalsFor: game.HomeGF,
+    goalsAgainst: game.HomeGA,
+    record: parseTeamRecord({ 
+      Wins: game.HomeWins, 
+      Losses: game.HomeLosses,
+      OTL: game.HomeOTL,
+      SOL: game.HomeSOL,
+    }),
+  }
+
   const parsedPrevMatchups = previousMatchups.map((game) => convertGameRowToGame(game));
  
   const response: Matchup = {
-    game: parsedGame,
+    game: {
+      season: game.SeasonID,
+      league: game.LeagueID,
+      date: game.Date,
+      homeTeam: game.Home,
+      homeScore: game.HomeScore,
+      awayTeam: game.Away,
+      awayScore: game.AwayScore,
+      type: game.Type,
+      played: game.Played,
+      overtime: game.Overtime,
+      shootout: game.Shootout,
+      slug: game.Slug
+    },
     teams: {
-      away: convertTeamRowToTeamIdentity(awayTeamData[0]),
-      home: convertTeamRowToTeamIdentity(homeTeamData[0]),
+      away: {
+        id: game.Away,
+        name: game.AwayName,
+        nickname: game.AwayNickname,
+        abbr: game.AwayAbbr,
+      },
+      home: {
+        id: game.Home,
+        name: game.HomeName,
+        nickname: game.HomeNickname,
+        abbr: game.HomwAbbr,
+      },
     },
     teamStats: {
       away: awayStats,
