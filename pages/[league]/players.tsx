9eab2da0287c57f1bbf729743d30de35f513c04b
Jason Phillips <jasphill52@gmail.com>
2021-04-11 20:50:00 -0700

    Players page updated with stats

--- pages/[league]/players.tsx
+++ pages/[league]/players.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import styled from 'styled-components';
 import { NextSeo } from 'next-seo';
@@ -8,10 +8,15 @@ import { NextSeo } from 'next-seo';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import useRatings from '../../hooks/useRatings';
+import useSkaterStats from '../../hooks/useSkaterStats';
+import useGoalieStats from '../../hooks/useGoalieStats';
 import useGoalieRatings from '../../hooks/useGoalieRatings';
 import SkaterRatingsTable from '../../components/RatingsTable/SkaterRatingsTable';
 import GoalieRatingsTable from '../../components/RatingsTable/GoalieRatingsTable';
-import { PlayerRatings, GoalieRatings } from '../..';
+import SkaterAdvStatTable from '../../components/PlayersTable/SkaterAdvStatTable';
+import SkaterStatsTable from '../../components/PlayersTable/SkaterStatsTable';
+import GoalieStatsTable from '../../components/PlayersTable/GoalieStatsTable';
+import { PlayerRatings, GoalieRatings, Player, Goalie } from '../..';
 
 interface Props {
   league: string;
@@ -19,9 +24,10 @@ interface Props {
 }
 
 function PlayerPage({ league }: Props): JSX.Element {
-  const { ratings: skaterratings, isLoading: isLoadingPlayers } = useRatings(
-    league
-  );
+  const { ratings: skaterratings, isLoading: isLoadingPlayers } = useRatings( league );
+  const { ratings: skater } = useSkaterStats( league );
+  const { ratings: goalie } = useGoalieStats( league );
+
   const {
     ratings: goalieratingdata,
     isLoading: isLoadingGoalies,
@@ -42,7 +48,23 @@ function PlayerPage({ league }: Props): JSX.Element {
           (player) => player.position === 'G'
         ) as Array<GoalieRatings>)
       : [];
+  
+  const getSkater = () =>
+  skater
+    ? (skater.filter(
+        (player) => player.position !== 'G'
+      ) as Array<Player>)
+    : [];
 
+  const getGoalie = () =>
+  goalie
+    ? (goalie.filter(
+        (player) => player.position !== 'G'
+      ) as Array<Goalie>)
+    : [];
+  
+      const [display, setDisplay] = useState('ratings');
+  
   return (
     <React.Fragment>
       <NextSeo
@@ -53,12 +75,48 @@ function PlayerPage({ league }: Props): JSX.Element {
       />
       <Header league={league} activePage="players" />
       <Container>
-        {/* Data for this page that we can also do: Roster, Historical Stats, etc. */}
+        <DisplaySelectContainer role="tablist">
+          <DisplaySelectItem
+            onClick={() => setDisplay(() => 'ratings')}
+            active={display === 'ratings'}
+            tabIndex={0}
+            role="tab"
+            aria-selected={display === 'ratings'}
+          >
+            Ratings
+          </DisplaySelectItem>
+          <DisplaySelectItem
+            onClick={() => setDisplay(() => 'stats')}
+            active={display === 'stats'}
+            tabIndex={0}
+            role="tab"
+            aria-selected={display === 'stats'}
+          >
+            Stats
+          </DisplaySelectItem>
+          <DisplaySelectItem
+            onClick={() => setDisplay(() => '')}
+            active={display === ''}
+            tabIndex={0}
+            role="tab"
+            aria-selected={display === ''}
+          >
+            Adv Stats
+          </DisplaySelectItem>
+        </DisplaySelectContainer>
         <TableHeading>Skaters</TableHeading>
         <TableWrapper>
           {!isLoadingPlayers && (
             <TableContainer>
-              <SkaterRatingsTable data={getSkaters()} />
+              {
+                display === 'ratings' ? (
+                  <SkaterRatingsTable data={getSkaters()} />
+                ) : display === 'stats' ? (
+                  <SkaterStatsTable data={getSkater()} />
+                ) : (
+                  <SkaterAdvStatTable data={getSkater()} />
+                )
+              }
             </TableContainer>
           )}
         </TableWrapper>
@@ -66,7 +124,13 @@ function PlayerPage({ league }: Props): JSX.Element {
         <TableWrapper>
           {!isLoadingGoalies && (
             <TableContainer>
-              <GoalieRatingsTable data={getGoalies()} />
+              {
+                display === 'ratings' ? (
+                  <GoalieRatingsTable data={getGoalies()} />
+                ) : (
+                  <GoalieStatsTable data={getGoalie()} />
+                )
+              }
             </TableContainer>
           )}
         </TableWrapper>
@@ -121,4 +185,24 @@ const TableHeading = styled.h2`
   font-size: 2.2rem;
   padding: 5px 0;
   border-bottom: 1px solid black;
+`;
+
+const DisplaySelectContainer = styled.div`
+  margin: 28px auto;
+  width: 95%;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey500};
+`;
+
+const DisplaySelectItem = styled.div<{ active: boolean }>`
+  display: inline-block;
+  padding: 8px 24px;
+  border: 1px solid
+    ${({ theme, active }) => (active ? theme.colors.grey500 : 'transparent')};
+  background-color: ${({ theme, active }) =>
+    active ? theme.colors.grey100 : 'transparent'};
+  border-radius: 5px 5px 0 0;
+  cursor: pointer;
+  position: relative;
+  border-bottom: none;
+  bottom: -1px;
 `;
