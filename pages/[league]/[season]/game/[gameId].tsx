91fd3840fc14a4a740be298e26f316732b3deb0d
Kenny Gilles <kenny.gilles@gmail.com>
2021-04-22 17:43:44 -0400

    Break out team stats widget to its own component

--- pages/[league]/[season]/game/[gameId].tsx
+++ pages/[league]/[season]/game/[gameId].tsx
@@ -9,8 +9,10 @@ import styled from 'styled-components';
 import { PulseLoader } from 'react-spinners';
 
 import Header from '../../../../components/Header';
+import { TeamStats } from '../../../../components/Game';
 import { Matchup as MatchupData, GoalieStats } from '../../../api/v1/schedule/game/[gameId]';
 import { Standings } from '../../../api/v1/standings';
+import { FlexColumn, FlexRow, SectionTitle, TeamLogoSmall } from '../../../../components/Game/common';
 
 interface Props {
   league: string;
@@ -63,58 +65,6 @@ function GameResults({ league, gameId }: Props): JSX.Element {
 
     setDivisions([awayDivision, homeDivision]);
   }, [divisionData, gameData, divisions]);
-
-  const renderTeamStats = () => {
-    const stats = {
-      goalsForPerGame: {
-        title: 'GF/GP',
-        away: {
-          total: 0,
-          perc: 0
-        },
-        home: {
-          total: 0,
-          perc: 0
-        }
-      },
-      goalsAgainstPerGame: {
-        title: 'GA/GP',
-        away: {
-          total: 0,
-          perc: 0
-        },
-        home: {
-          total: 0,
-          perc: 0
-        }
-      }
-    };
-    const awayColor = gameData.teams.away.primaryColor;
-    const homeColor = gameData.teams.home.primaryColor;
-    Object.keys(gameData.teamStats).forEach((team) => {
-      stats.goalsForPerGame[team].total = parseFloat((gameData.teamStats[team].goalsFor / gameData.teamStats[team].gamesPlayed).toFixed(2));
-      stats.goalsAgainstPerGame[team].total = parseFloat((gameData.teamStats[team].goalsAgainst / gameData.teamStats[team].gamesPlayed).toFixed(2));
-    });
-    stats.goalsForPerGame.away.perc = stats.goalsForPerGame.away.total / (stats.goalsForPerGame.away.total + stats.goalsForPerGame.home.total) * 100;
-    stats.goalsAgainstPerGame.away.perc = stats.goalsAgainstPerGame.home.total / (stats.goalsAgainstPerGame.away.total + stats.goalsAgainstPerGame.home.total) * 100;
-
-    return (
-      <>
-        <TeamStatsRow>
-          <StatValue>{stats.goalsForPerGame.away.total}</StatValue>
-          <StatTitle>{stats.goalsForPerGame.title}</StatTitle>
-          <StatValue home>{stats.goalsForPerGame.home.total}</StatValue>
-        </TeamStatsRow>
-        <TeamStatsBar awayColor={awayColor} awayPerc={stats.goalsForPerGame.away.perc} homeColor={homeColor} />
-        <TeamStatsRow>
-          <StatValue>{stats.goalsAgainstPerGame.away.total}</StatValue>
-          <StatTitle>{stats.goalsAgainstPerGame.title}</StatTitle>
-          <StatValue home>{stats.goalsAgainstPerGame.home.total}</StatValue>
-        </TeamStatsRow>
-        <TeamStatsBar awayColor={awayColor} awayPerc={stats.goalsAgainstPerGame.away.perc} homeColor={homeColor} />
-      </>
-    )
-  };
 
   const renderDivisionStandings = () => {
     const isSameDivision = divisions[0].name === divisions[1].name;
@@ -391,22 +341,7 @@ function GameResults({ league, gameId }: Props): JSX.Element {
         {!isLoading && (
           <>
             <FlexColumn width={300}>
-              <TeamStats>
-                <TeamStatsHeader>
-                  <FlexRow height={50}>
-                    <TeamLogoSmall>
-                      <Sprites.Away />
-                    </TeamLogoSmall>
-                    <SectionTitle>
-                      Team Stats
-                    </SectionTitle>
-                    <TeamLogoSmall>
-                      <Sprites.Home />
-                    </TeamLogoSmall>
-                  </FlexRow>
-                </TeamStatsHeader>
-                {renderTeamStats()}
-              </TeamStats>
+              <TeamStats gameData={gameData} Sprites={Sprites} />
               {isRegularSeason && divisionError &&
                 <ErrorBlock>
                   Failed to load division standings
@@ -456,67 +391,7 @@ const Container = styled.div`
   }
 `;
 
-const FlexRow = styled.div<{
-  height?: number;
-}>`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  height: ${({ height }) => height ? `${height}px` : 'auto'}
-`;
-
-const FlexColumn = styled.div<{
-  width?: number;
-}>`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: ${({ width }) => width ? `${width}px` : '100%'};
-  height: fit-content;
-`;
-
-const SectionTitle = styled.span`
-  font-weight: 600;
-`;
-
 // Left
-const TeamStats = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.colors.grey100};
-  padding: 15px;
-`;
-
-const TeamStatsHeader = styled.div`
-  font-weight: 600;
-  border-bottom: 2px solid ${({ theme }) => theme.colors.grey300};
-`;
-
-const TeamStatsRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 15px;
-  padding-bottom: 5px;
-`;
-
-const TeamStatsBar = styled.div<{
-  awayColor: string;
-  awayPerc: number;
-  homeColor: string;
-}>`
-  border: 2px solid;
-  border-image: ${props => `
-    linear-gradient(to right,
-      ${props.awayColor} 0%,
-      ${props.awayColor} ${props.awayPerc}%,
-      ${props.theme.colors.grey100} ${props.awayPerc}%,
-      ${props.theme.colors.grey100} ${props.awayPerc+1}%,
-      ${props.homeColor} ${props.awayPerc+1}%,
-      ${props.homeColor} 100%
-    ) 5`};
-`;
-
 const DivisionStandings = styled.div`
   background-color: ${({ theme }) => theme.colors.grey100};
   margin-top: 10px;
@@ -582,20 +457,6 @@ const TeamsPreview = styled.div`
   padding: 0 15px 15px 15px;
 `;
 
-const StatTitle = styled.span`
-  font-size: 16px;
-`;
-
-const StatValue = styled.span<{
-  home?: boolean;
-}>`
-  font-family: Montserrat, sans-serif;
-  font-size: 22px;
-  font-weight: 700;
-  flex: 1;
-  ${({ home }) => home && 'text-align: right;'};
-`;
-
 const TeamData = styled.div`
   display: flex;
   flex-direction: row;
@@ -626,11 +487,6 @@ const TeamRecord = styled.span<{
   text-align: ${({ home }) => home ? 'right' : 'left'};
   padding: 0 10px;
   color: ${({ theme }) => theme.colors.grey600};
-`;
-
-const TeamLogoSmall = styled.div`
-  width: 25px;
-  height: 25px;
 `;
 
 const TeamLogo = styled.div`
