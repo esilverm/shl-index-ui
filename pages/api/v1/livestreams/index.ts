ac22126ff10e7d91e0cb0c01ccbddfc83a885514
Nick Flint <nick.r.flint@gmail.com>
2021-05-05 08:23:24 -0400

    Add api endpoint for livestreams

---
+++ pages/api/v1/livestreams/index.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import SQL from 'sql-template-strings';
+import Cors from 'cors';
+import { query } from '../../../../lib/db';
+import use from '../../../../lib/middleware';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+type Data = Array<{
+  id: number;
+  league: string;
+  videoId: string;
+  isLive: string;
+}>;
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data | string>
+): Promise<void> => {
+  await use(req, res, cors);
+
+  const { league = null} = req.query;
+
+  const search = SQL`SELECT * FROM youtube_data`
+    .append(
+        league != null
+        ? SQL`
+        WHERE league=${league}
+        `
+        : ''
+    );
+
+  const youtube_data: Array<{
+      id: number,
+      league: string,
+      videoId: string,
+      isLive: string
+  }> = await query(search);
+
+  if (youtube_data.length === 0) {
+    res
+      .status(404)
+      .send('Error 404: Could not find youtube data for given parameters.');
+    return;
+  }
+
+  const parsed = youtube_data.map((data) => ({
+    id: data.id,
+    league: data.league,
+    videoId: data.videoId,
+    isLive: data.isLive
+  }));
+
+  res.status(200).json(parsed);
+};
