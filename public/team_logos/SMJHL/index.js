ca77eda5060993d9ba4042c3db018cf9f3fc4204
Jason Phillips <jasphill52@gmail.com>
2021-08-23 22:54:42 -0700

    Add GFG and REG Logos for SMJHL Expansion

--- public/team_logos/SMJHL/index.js
+++ public/team_logos/SMJHL/index.js
@@ -3,11 +3,13 @@ import ANC from './Anchorage.svg?sprite';
 import CAR from './Carolina.svg?sprite';
 import COL from './Colorado.svg?sprite';
 import DET from './Detroit.svg?sprite';
+import GFG from './Great_Falls.svg?sprite';
 import KEL from './Kelowna.svg?sprite';
 import MET from './Maine.svg?sprite';
 import NBB from './Nevada.svg?sprite';
 import NL from './Newfoundland.svg?sprite';
 import QCC from './Quebec_City.svg?sprite';
+import REG from './Regina.svg?sprite';
 import STL from './St_Louis.svg?sprite';
 import STLOld from './St_LouisOld.svg?sprite';
 import VAN from './Vancouver.svg?sprite';
@@ -18,11 +20,13 @@ export default {
   CAR,
   COL,
   DET,
+  GFG,
   KEL,
   MET,
   NBB,
   NL,
   QCC,
+  REG,
   STL,
   STLOld,
   VAN,
