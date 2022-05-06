c65b6c521aa0338d6d380fed7b2dffb3481b544d
Evan Silverman <es4753@nyu.edu>
2022-05-06 12:19:13 -0400

    initialize query params and linking

---
+++ utils/seasonType.ts
@@ -0,0 +1,7 @@
+export const getQuerySeasonType = (): string => {
+  if (typeof window === 'undefined') return '';
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const seasonType = urlParams.get('type') || '';
+  return seasonType.match(/\w+/) ? seasonType : '';
+};
